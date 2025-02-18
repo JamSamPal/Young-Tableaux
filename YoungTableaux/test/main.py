22138b8f397b15a08fb41d14b7a5e8e3b23995ee
James Pallister <jamespallister86@gmail.com>
2025-02-18 21:12:19 +0000

    Added Tests, corrected results

---
+++ YoungTableaux/test/main.py
@@ -0,0 +1,10 @@
+from test import YoungTableauxTest
+
+def main():
+    print("Starting test...")
+    YoungTableauxTest(4)
+    YoungTableauxTest(5)
+    YoungTableauxTest(20)
+
+if __name__ == "__main__":
+    main()
